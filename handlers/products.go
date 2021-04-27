385729487ad1bac5ad9e88508f0b7a449703626b
manunio <max.manu.nair@gmail.com>
2021-04-27 20:32:16 +0530

    abstract handler funs and started implementing restfull endpoints

--- handlers/products.go
+++ handlers/products.go
@@ -1,25 +1,46 @@
 package handlers
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"product-service/data"
 )
 
+// Products is a http.Handler
 type Products struct {
 	l *log.Logger
 }
 
+// NewProducts creates a product handler with the given logger
 func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p *Products) ServeHTTP(rw http.ResponseWriter, h *http.Request) {
+// ServeHTTP is the main entry point for the handler and satisfies the http.Handler
+// interface
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	// handle the request for list of products
+
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+
+	// catch all
+	// if not method is satisfied return an error
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+// getProducts returns the products from the data store
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
+
+	// fetch the products from the datastore
 	lp := data.GetProducts()
-	d, err := json.Marshal(lp)
+
+	// serialize the list to JSON
+	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
-	rw.Write(d)
 }
